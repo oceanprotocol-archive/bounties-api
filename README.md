e1b667297a874f9951d611f447f7cf124d954260
Matthias Kretschmann <m@kretschmann.io>
2019-01-14 23:41:55 +0100

    drop da base 🍸

---
+++ README.md
@@ -0,0 +1,69 @@
+[![banner](https://raw.githubusercontent.com/oceanprotocol/art/master/github/repo-banner%402x.png)](https://oceanprotocol.com)
+
+<h1 align="center">bounties-api</h1>
+
+> Microservice to cache and expose Bounty data from Gitcoin & Bounties.network for use throughout [oceanprotocol.com](https://oceanprotocol.com).
+
+[![Build Status](https://travis-ci.com/oceanprotocol/bounties-api.svg?branch=master)](https://travis-ci.com/oceanprotocol/bounties-api)
+[![js oceanprotocol](https://img.shields.io/badge/js-oceanprotocol-7b1173.svg)](https://github.com/oceanprotocol/eslint-config-oceanprotocol)
+[![Greenkeeper badge](https://badges.greenkeeper.io/oceanprotocol/bounties-api.svg)](https://greenkeeper.io/)
+<img src="http://forthebadge.com/images/badges/powered-by-electricity.svg" height="20"/>
+<img src="http://forthebadge.com/images/badges/as-seen-on-tv.svg" height="20"/>
+<img src="http://forthebadge.com/images/badges/uses-badges.svg" height="20"/>
+
+## API
+
+Endpoint: [`https://oceanprotocol-bounties.now.sh`](https://oceanprotocol-bounties.now.sh)
+
+### GET /
+
+**200**: Returns the count of open bounties, and a `total` number counting all active and completed bounties:
+
+```json
+{
+  "gitcoin": 1,
+  "bountiesNetwork": 2,
+  "total": 17
+}
+```
+
+## Development
+
+Install dependencies:
+
+```bash
+npm install
+```
+
+And run the server:
+
+```bash
+npm run dev
+```
+
+## Test
+
+Run the tests:
+
+```bash
+npm test
+```
+
+## Deployment
+
+Deploy to [now](https://zeit.co/now), make sure to switch to Ocean Protocol org before deploying:
+
+```bash
+# first run
+now login
+now switch
+
+# deploy
+now
+# switch alias to new deployment
+now alias
+```
+
+## Authors
+
+- Matthias Kretschmann ([@kremalicious](https://github.com/kremalicious)) - [Ocean Protocol](https://oceanprotocol.com)
---
+++ index.js
@@ -0,0 +1,72 @@
+const ms = require('ms')
+const fetch = require('node-fetch')
+
+let cacheGitcoin = null
+let cacheBountiesNetwork = null
+let cacheTotal = null
+
+const getGitcoin = async () => {
+    const response = await fetch('https://gitcoin.co/api/v0.1/bounties/')
+
+    const gitcoinBody = await response.json() // returns only open bounties by default
+
+    const gitcoin = await gitcoinBody.filter(
+        // filter the response manually, no way atm to do that as API query
+        item => item.funding_organisation.includes('Ocean Protocol')
+    )
+
+    return {
+        data: gitcoin.length,
+        lastUpdate: Date.now()
+    }
+}
+
+const getBountiesNetwork = async () => {
+    const response = await fetch('https://api.bounties.network/bounty/?search=ocean%20protocol&bountyStage=1&platform=bounties-network')
+
+    const bountiesNetwork = await response.json()
+
+    return {
+        data: bountiesNetwork.results.length,
+        lastUpdate: Date.now()
+    }
+}
+
+const getTotal = async () => {
+    const response = await fetch('https://api.bounties.network/bounty/?search=ocean%20protocol')
+
+    const allBounties = await response.json()
+
+    return {
+        data: allBounties.count,
+        lastUpdate: Date.now()
+    }
+}
+
+//
+// Create the response
+//
+module.exports = async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET')
+
+    if (!cacheGitcoin || Date.now() - cacheGitcoin.lastUpdate > ms('5m')) {
+        cacheGitcoin = await getGitcoin()
+    }
+
+    if (!cacheBountiesNetwork || Date.now() - cacheBountiesNetwork.lastUpdate > ms('5m')) {
+        cacheBountiesNetwork = await getBountiesNetwork()
+    }
+
+    if (!cacheTotal || Date.now() - cacheTotal.lastUpdate > ms('5m')) {
+        cacheTotal = await getTotal()
+    }
+
+    const response = {
+        'gitcoin': cacheGitcoin.data,
+        'bountiesNetwork': cacheBountiesNetwork.data,
+        'total': cacheTotal.data
+    }
+
+    res.end(JSON.stringify(response))
+}
